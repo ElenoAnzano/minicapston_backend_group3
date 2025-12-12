47f0426260f69177104ca92de4d1ee9c99ccb107
ElenoAnzano <elenoanzanojr123@gmail.com>
2025-12-12 11:30:43 +0800

    Update loginRoutes.ts

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { register, login, resetPassword } from "@/controllers/loginController";
+import { register, login, resetPassword } from "../controllers/loginController";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/reset-password", resetPassword);  
+
 
 export default router;
