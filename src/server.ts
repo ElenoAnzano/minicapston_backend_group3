46cf1066fc84d3fbf21e9af450455ed76a5d99a9
ElenoAnzano <elenoanzanojr123@gmail.com>
2025-12-12 16:48:06 +0800

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -11,14 +11,6 @@ dotenv.config();
 const port = process.env.PORT ? Number(process.env.PORT) : 5000;
 const server = http.createServer(app);
 
-const app = express();
-app.use(
-  cors({
-    origin: "https://eventmanagement-taupe.vercel.app",
-    credentials: true,
-  })
-);
-
 const io = new SocketIOServer(server, {
   cors: {
     origin: process.env.NODE_ENV === "production"
@@ -35,6 +27,7 @@ server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 
 });
+
 
 
 
