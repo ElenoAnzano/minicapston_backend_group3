ea02f62fe72937772466d1ef051d717df355eeea
ElenoAnzano <elenoanzanojr123@gmail.com>
2025-12-12 11:14:43 +0800

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import router from "@/routes";
+import router from "../src/routes";
 import loginRoutes from "@/routes/loginRoutes";
 import profileRoutes from "@/routes/profileRoutes";
 
@@ -26,5 +26,6 @@ app.use("/api/login", loginRoutes);
 
 //profile
 app.use("/api/profile", profileRoutes);
+
 
 export default app;
