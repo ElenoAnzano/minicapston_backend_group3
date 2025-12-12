57d3a770a0ae2af4cf3407027f82c3e04b0fd681
ElenoAnzano <elenoanzanojr123@gmail.com>
2025-12-12 15:45:12 +0800

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 const io = new SocketIOServer(server, {
   cors: {
     origin: process.env.NODE_ENV === "production"
-        ? ["https://your-frontend.vercel.app", "https://minicapston-frontend.up.railway.app"] // ← put your real frontend URLs here
+        ? ["https://your-frontend.vercel.app", "https://eventmanagement-taupe.vercel.app/login"] 
         : "http://localhost:5173",
     credentials: true,
   },
@@ -26,4 +26,5 @@ server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 
 });
+
 
